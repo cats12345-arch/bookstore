07a1f6415c562da874c6f3e1cc5db5f4bf75233c
Daniel Moss <danielpmoss123@gmail.com>
2025-10-25 12:28:02 -0400

    changes to make a static github page

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import Book from './pages/Book';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />}/>
